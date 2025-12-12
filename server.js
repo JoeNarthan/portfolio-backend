f3d9bd6dbbc58e2afa5d7aa11722000ebe461620
JoeNarthan <77740552+JoeNarthan@users.noreply.github.com>
2025-12-12 17:16:13 +0700

    use correct database url

--- server.js
+++ server.js
@@ -41,5 +41,8 @@ app.get("/comments", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Backend running on port " + PORT));
-// test redeploy
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
