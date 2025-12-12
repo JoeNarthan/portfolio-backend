6ac75641bd549ee685f3fdaac9fede8c91358e29
JoeNarthan <77740552+JoeNarthan@users.noreply.github.com>
2025-12-12 16:44:43 +0700

    Remove mysql db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const db = await mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.root,
-    password: process.env.visal1122,
-    database: process.env.DB_NAME
-})
